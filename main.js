26234292884ba495127aa51cc0d4ea7bb8116d9d
SUTHAR ZEEL <165781076+Jeelsuthar@users.noreply.github.com>
2024-06-30 22:07:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ["Front-end Developer", 'Web Developer'],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true, // Default value
+});
